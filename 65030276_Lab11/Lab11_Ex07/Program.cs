8867b2487712c50e9da738b1e3878d607c9b7446
anndyyz <anda2546r@gmail.com>
2024-03-30 22:24:30 +0700

    ex07

---
+++ 65030276_Lab11/Lab11_Ex07/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Base
+{
+    public virtual void A()
+    {
+        Console.WriteLine("Base.A()");
+    }
+}
+
+class Derived_1 : Base
+{
+    public sealed override void A()
+    {
+        Console.WriteLine("Derived_1.A()");
+    }
+}
+
+class Derived_2 : Derived_1
+{
+    public new void A()  // ไม่ได้ใช้คำสงวน override
+    {
+        Console.WriteLine("Derived_2.A()");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Derived_2 d2 = new Derived_2();
+        Base b = d2; // ไม่จำเป็นต้องใช้ casting
+        Derived_1 d1 = d2; // ไม่จำเป็นต้องใช้ casting
+
+        b.A();
+        d1.A();
+        d2.A();
+    }
+}
