fb5ec559623d731eba52aca75f3e62cddb899ca9
anndyyz <anda2546r@gmail.com>
2024-03-30 22:12:48 +0700

    ex2

---
+++ 65030276_Lab11/Lab11_Ex02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public static class Calculator
+{
+    public static int AddValues(int a, int b)
+    {
+        Console.WriteLine("Calculated by method: public static int AddValues(int a, int b)");
+        return a + b;
+    }
+    
+    public static float AddValues(float c, float d)
+    {
+        Console.WriteLine("Calculated by method: public static float AddValues(float c, float d)");
+        return c + d;
+    }
+    
+    public static long AddValues(long e, long f)
+    {
+        Console.WriteLine("Calculated by method: public static long AddValues(long e, long f)");
+        return e + f;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Calculator.AddValues(2, 3));
+        Console.WriteLine(Calculator.AddValues(2.0f, 3.0f));
+        Console.WriteLine(Calculator.AddValues(2L, 3L));
+    }
+}
