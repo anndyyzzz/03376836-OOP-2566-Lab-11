6ff46aca5d7be7ebe5f4fe6c6d1fa09c4b10b235
anndyyz <anda2546r@gmail.com>
2024-03-30 22:15:22 +0700

    ex3

--- 65030276_Lab11/Lab11_Ex01/obj/Debug/net8.0/Lab11_Ex01.AssemblyInfo.cs
+++ 65030276_Lab11/Lab11_Ex01/obj/Debug/net8.0/Lab11_Ex01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab11_Ex01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28820002e353bf082846bcbab6218d4e8001230e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa7a16b7ab7927be6b0492f8bddb3055e388ecd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab11_Ex01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab11_Ex01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
